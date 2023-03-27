a3f672ec6e20648042f462d8cb49e7c2a2db1639
dunkan_high <oleg.savinov.2002@bk.ru>
2023-03-27 18:56:44 +0300

    add login_page tests

--- test_main_page.py
+++ test_main_page.py
@@ -8,7 +8,7 @@ def go_to_login_page(browser):
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
